198d8ec6235ffcee0daeb56c2e0fefde97489453
therealrk <43609678+therealrk@users.noreply.github.com>
2020-08-09 20:26:33 +0800

    Update Player movement by getkey

---
+++ Assets/C# Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //will be replace the get key to touch
+    [Range(-3, 3)] public float value;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(value, transform.position.y, transform.position.z);
+    }
+}
