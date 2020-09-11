2da4eed3a62850f899d8045bbfc72c80816e6697
Kaz <kazt452@gmail.com>
2020-09-11 10:44:23 +0800

    basic movement

--- Assets/C# Scripts/PlayerMovement.cs
+++ Assets/C# Scripts/PlayerMovement.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private CharacterController controller;
+    private Vector3 direction;
     //will be replace the get key to touch
+    public float forwardSpeed = 5f;
+    public float jumpForce = 10f;
+    public float Gravity = -20;
     [Range(-3, 3)] public float value;
 
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        direction.z = forwardSpeed;
+        direction.y += Gravity * Time.deltaTime;
     }
 
     void FixedUpdate()
     {
+        controller.Move(direction * Time.fixedDeltaTime);
         transform.position = new Vector3(value, transform.position.y, transform.position.z);
 
         if (Input.GetButtonDown("Right"))
@@ -32,5 +44,18 @@ public class PlayerMovement : MonoBehaviour
             }
             value -= 3;
         }
+
+        if (controller.isGrounded)
+        {
+            if (Input.GetButtonDown("Up"))
+            {
+                Jump();
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        direction.y = jumpForce;
     }
 }
