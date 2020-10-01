aeea6c2a0ddeb98882e51b911a3ffad1d519aa77
therealrk <43609678+therealrk@users.noreply.github.com>
2020-10-01 20:06:14 +0800

    Update scoresystem and UI adjustment

---
+++ Assets/C# Scripts/ScoreSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSystem : MonoBehaviour
+{
+    public Text scoreCurrent;
+    public Text scoreFinal;
+    public float scoreAmount;
+    public float pointIncreasedPerSec;
+    public PlayerUI PUI;
+
+    void Start()
+    {
+        scoreAmount = 0f;
+        pointIncreasedPerSec = 10f;
+    }
+
+    void Update()
+    {
+        if (PUI.GameOver == false)
+        {
+            scoreCurrent.text = (int)scoreAmount + "KM";
+            scoreAmount += pointIncreasedPerSec * Time.deltaTime;
+        }
+        if (PUI.GameOver == true)
+        {
+            scoreFinal.text = scoreCurrent.text;
+            Debug.Log(scoreFinal + "Stop");
+        }
+    }
+}
