015bb064f80368d785dfd14c2f6002dfdc5e8406
therealrk <43609678+therealrk@users.noreply.github.com>
2020-10-01 20:52:33 +0800

    random spawn hongkong prefab

--- Assets/C# Scripts/TileManager.cs
+++ Assets/C# Scripts/TileManager.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class TileManager : MonoBehaviour
 {
     public GameObject[] tilePrefabs;
+    public GameObject[] secondTilePrefabs;
     public float zSpawn = 0;
     public float tileLength = 30;
     public int numberOfTiles = 5;
+    public ScoreSystem scoreSystem;
     [SerializeField] private List<GameObject> activeTiles = new List<GameObject>();
     public Transform playerTranform;
     // Start is called before the first frame update
@@ -31,9 +33,21 @@ public class TileManager : MonoBehaviour
 
     public void SpawnTile(int tileIndex)
     {
-        GameObject go =Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
-        activeTiles.Add(go);
-        zSpawn += tileLength;
+        if (scoreSystem.scoreAmount < 30000)
+        {
+            GameObject go = Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+            activeTiles.Add(go);
+            zSpawn += tileLength;
+            Debug.Log("HongKongPrefab");
+        }
+        if (scoreSystem.scoreAmount >= 30000)
+        {
+            GameObject go_02 = Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+            activeTiles.Add(go_02);
+            zSpawn += tileLength;
+            Debug.Log("CyberpunkPrefab");
+        }
+
     }
 
     private void DeleteTile()
