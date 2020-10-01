f8804aae814386bb2d9027cc56be6dbf6247e20f
therealrk <43609678+therealrk@users.noreply.github.com>
2020-10-02 02:06:50 +0800

    generate different lv (error)

--- Assets/C# Scripts/TileManager.cs
+++ Assets/C# Scripts/TileManager.cs
@@ -33,16 +33,16 @@ public class TileManager : MonoBehaviour
 
     public void SpawnTile(int tileIndex)
     {
-        if (scoreSystem.scoreAmount < 30000)
+        if (scoreSystem.scoreAmount < 50)
         {
             GameObject go = Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
             activeTiles.Add(go);
             zSpawn += tileLength;
             Debug.Log("HongKongPrefab");
         }
-        if (scoreSystem.scoreAmount >= 30000)
+        if (scoreSystem.scoreAmount > 50)
         {
-            GameObject go_02 = Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+            GameObject go_02 = Instantiate(secondTilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
             activeTiles.Add(go_02);
             zSpawn += tileLength;
             Debug.Log("CyberpunkPrefab");
