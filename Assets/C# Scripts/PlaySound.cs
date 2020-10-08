6c16445af61d10a77b95572a3324c397a6709fbd
Basstruney <61958902+Basstruney@users.noreply.github.com>
2020-10-09 01:43:27 +0800

    added sounds

---
+++ Assets/C# Scripts/PlaySound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioSource sound_click;
+    public AudioSource sound_confirm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playClickSound()
+    {
+        sound_click.Play();
+    }
+
+    public void playConfirmSound()
+    {
+        sound_confirm.Play();
+    }
+}
